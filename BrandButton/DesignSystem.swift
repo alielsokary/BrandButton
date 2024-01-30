76c9d991066766c2c1deaffa6e6eab2216512bcd
Ali Elsokary <ali.elsokary.w@gmail.com>
2024-01-30 21:21:19 +0200

    Add Color Tokens from Figma and Create Design System with the color tokens

---
+++ BrandButton/DesignSystem.swift
@@ -0,0 +1,31 @@
+//
+//  DesignSystem.swift
+//  BrandButton
+//
+//  Created by Ali Elsokary on 30/01/2024.
+//
+
+import UIKit
+
+struct DS {
+    
+    enum Colors {
+        
+        enum Button {
+            static let defaultGreen = UIColor(named: "trvGreen700")!
+            static let highlightedGreen = UIColor(named: "trvGreen900")!
+            static let backgroundGreen = UIColor(named: "trvGreen100")!
+
+            
+            static let defaultBlue = UIColor(named: "trvBlue700")!
+            static let highlightedBlue = UIColor(named: "trvBlue900")!
+            static let backgroundBlue = UIColor(named: "trvBlue100")!
+
+            static let backgroundDisabled = UIColor(named: "trvJuri300")!
+            
+            static let neutralsWhite = UIColor(named: "neutralsWhite")!
+
+
+        }
+    }
+}
