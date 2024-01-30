3fc9132309ceed7ca6807bb54e3469c5cc439d87
Ali Elsokary <ali.elsokary.w@gmail.com>
2024-01-30 18:25:53 +0200

    Create BrandButton custom UIButton

---
+++ BrandButton/BrandButton.swift
@@ -0,0 +1,20 @@
+//
+//  BrandButton.swift
+//  BrandButton
+//
+//  Created by Ali Elsokary on 30/01/2024.
+//
+
+import UIKit
+
+class BrandButton: UIButton {
+    enum ButtonStyle {
+        case primary
+        case secondary
+    }
+    
+    enum ColorStyle {
+        case mainGreen
+        case mainBlue
+    }
+}
