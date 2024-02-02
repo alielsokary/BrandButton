867d5a15beb49848411961ac16ef60a16d18ebb1
Ali Elsokary <ali.elsokary.w@gmail.com>
2024-02-03 00:28:56 +0300

    Refactor updateButtonStyle function and updateColorStyle function

--- BrandButton/BrandButton.swift
+++ BrandButton/BrandButton.swift
@@ -36,13 +36,13 @@ class BrandButton: UIButton {
     
     override var isEnabled: Bool {
         didSet {
-            updateButtonStyle()
+            updateButtonState()
         }
     }
     
     override var isHighlighted: Bool {
         didSet {
-            updateButtonStyle()
+            updateButtonState()
         }
     }
     
@@ -64,13 +64,13 @@ class BrandButton: UIButton {
     
     var variant: Variant = .primary {
         didSet {
-            updateButtonStyle()
+            updateButtonState()
         }
     }
     
     var style: Style = .mainGreen {
         didSet {
-            updateColorStyle()
+            updateButtonState()
         }
     }
     
@@ -78,72 +78,103 @@ class BrandButton: UIButton {
     
     var fullWidth: Bool = false
     
-    // MARK: Properties for default state
-    private var defaultBackgroundColor: UIColor = DS.Colors.Button.defaultGreen {
-        didSet {
-            updateButtonStyle()
-        }
-    }
-    
-    private var defaultBorderColor: UIColor = .clear {
-        didSet {
-            updateButtonStyle()
-        }
-    }
-    
-    private var defaultTitleColor: UIColor = DS.Colors.Button.neutralsWhite {
-        didSet {
-            updateButtonStyle()
-        }
-    }
-    
-    // MARK: Properties for highlighted state
-    private var highlightedBackgroundColor: UIColor = DS.Colors.Button.backgroundGreen {
-        didSet {
-            updateButtonStyle()
-        }
-    }
-    
-    private var highlightedBorderColor: UIColor = DS.Colors.Button.highlightedGreen {
-        didSet {
-            updateButtonStyle()
-        }
-    }
-    
-    private var highlightedTitleColor: UIColor = DS.Colors.Button.highlightedGreen {
-        didSet {
-            updateButtonStyle()
-        }
-    }
-    
-    // MARK: Properties for disabled state
-    private var disabledBackgroundColor: UIColor = DS.Colors.Button.backgroundDisabled {
-        didSet {
-            updateButtonStyle()
-        }
-    }
-    
-    private var disabledBorderColor: UIColor = .clear {
-        didSet {
-            updateButtonStyle()
-        }
-    }
-    
-    private var disabledTitleColor: UIColor = DS.Colors.Button.neutralsWhite {
-        didSet {
-            updateButtonStyle()
-        }
-    }
-    
     var leadingIcon: UIImage? {
         didSet {
-            updateButtonStyle()
+            updateButtonState()
         }
     }
     
     var trailingIcon: UIImage? {
         didSet {
-            updateButtonStyle()
+            updateButtonState()
+        }
+    }
+    
+    // MARK: Properties for default state
+    private var defaultBackgroundColor: UIColor {
+        switch style {
+        case .mainGreen:
+            switch variant {
+            case .primary:
+                return DS.Colors.Button.defaultGreen
+            case .secondary:
+                return DS.Colors.Button.neutralsWhite
+            }
+        case .mainBlue:
+            switch variant {
+            case .primary:
+                return DS.Colors.Button.defaultBlue
+            case .secondary:
+                return DS.Colors.Button.neutralsWhite
+            }
+        }
+    }
+    
+    private var defaultBorderColor: UIColor {
+        switch style {
+        case .mainGreen:
+            return variant == .primary ? .clear : DS.Colors.Button.defaultGreen
+        case .mainBlue:
+            return variant == .primary ? .clear : DS.Colors.Button.defaultBlue
+        }
+    }
+    
+    private var defaultTitleColor: UIColor {
+        switch style {
+        case .mainGreen:
+            return variant == .primary ? DS.Colors.Button.neutralsWhite : DS.Colors.Button.defaultGreen
+        case .mainBlue:
+            return variant == .primary ? .white : DS.Colors.Button.defaultBlue
+        }
+    }
+    
+    // MARK: Properties for highlighted state
+    private var highlightedBackgroundColor: UIColor {
+        switch style {
+        case .mainGreen:
+            return variant == .primary ? DS.Colors.Button.highlightedGreen : DS.Colors.Button.backgroundGreen
+        case .mainBlue:
+            return variant == .primary ? DS.Colors.Button.highlightedBlue : DS.Colors.Button.backgroundBlue
+        }
+    }
+    
+    private var highlightedBorderColor: UIColor {
+        switch style {
+        case .mainGreen:
+            return variant == .primary ? .clear : DS.Colors.Button.highlightedGreen
+        case .mainBlue:
+            return variant == .primary ? .clear : DS.Colors.Button.highlightedBlue
+        }
+    }
+    
+    private var highlightedTitleColor: UIColor {
+        switch style {
+        case .mainGreen:
+            return variant == .primary ? DS.Colors.Button.neutralsWhite : DS.Colors.Button.highlightedGreen
+        case .mainBlue:
+            return variant == .primary ? .white : DS.Colors.Button.highlightedBlue
+        }
+    }
+    
+    // MARK: Properties for disabled state
+    private var disabledBackgroundColor: UIColor {
+        switch style {
+        case .mainGreen, .mainBlue:
+            return variant == .primary ? DS.Colors.Button.backgroundDisabled : DS.Colors.Button.neutralsWhite
+        }
+    }
+    
+    private var disabledBorderColor: UIColor {
+        switch style {
+        case .mainGreen, .mainBlue:
+            return variant == .primary ? .clear : DS.Colors.Button.backgroundDisabled
+        }
+    }
+    
+    private var disabledTitleColor: UIColor {
+        switch style {
+        case .mainGreen, .mainBlue:
+            return variant == .primary ? DS.Colors.Button.neutralsWhite : DS.Colors.Button.backgroundDisabled
         }
     }
     
@@ -166,44 +197,49 @@ class BrandButton: UIButton {
         layer.borderWidth = 1
         clipsToBounds = true
         
-        updateButtonStyle()
+        updateButtonState(withAnimation: false)
     }
     
-    private func updateButtonStyle() {
-        UIView.animate(withDuration: 0.2) { [weak self] in
-            guard let self = self else { return }
-
-            switch (self.variant, self.isEnabled, self.isHighlighted) {
-            case (.primary, true, false):
-                self.applyStyle(backgroundColor: self.defaultBackgroundColor,
-                                borderColor: self.defaultBorderColor,
-                                titleColor: self.defaultTitleColor)
-            case (.primary, true, true):
-                self.applyStyle(backgroundColor: self.highlightedBackgroundColor,
-                                borderColor: self.highlightedBorderColor,
-                                titleColor: self.highlightedTitleColor)
-            case (.primary, false, _):
-                self.applyStyle(backgroundColor: self.disabledBackgroundColor,
-                                borderColor: self.disabledBorderColor,
-                                titleColor: self.disabledTitleColor)
-
-            case (.secondary, true, false):
-                self.applyStyle(backgroundColor: self.defaultBackgroundColor,
-                                borderColor: self.defaultBorderColor,
-                                titleColor: self.defaultTitleColor)
-            case (.secondary, true, true):
-                self.applyStyle(backgroundColor: self.highlightedBackgroundColor,
-                                borderColor: self.highlightedBorderColor,
-                                titleColor: self.highlightedTitleColor)
-            case (.secondary, false, _):
-                self.applyStyle(backgroundColor: self.disabledBackgroundColor,
-                                borderColor: self.disabledBorderColor,
-                                titleColor: self.disabledTitleColor)
+    private func updateButtonState(withAnimation animation: Bool = true) {
+        if animation {
+            UIView.animate(withDuration: 0.2) { [weak self] in
+                guard let self = self else { return }
+                setButtonState()
             }
         }
     }
     
-    private func applyStyle(backgroundColor: UIColor, borderColor: UIColor, titleColor: UIColor) {
+    private func setButtonState() {
+        switch (self.variant, self.isEnabled, self.isHighlighted) {
+        case (.primary, true, false):
+            self.setButtonWith(backgroundColor: self.defaultBackgroundColor,
+                            borderColor: self.defaultBorderColor,
+                            titleColor: self.defaultTitleColor)
+        case (.primary, true, true):
+            self.setButtonWith(backgroundColor: self.highlightedBackgroundColor,
+                            borderColor: self.highlightedBorderColor,
+                            titleColor: self.highlightedTitleColor)
+        case (.primary, false, _):
+            self.setButtonWith(backgroundColor: self.disabledBackgroundColor,
+                            borderColor: self.disabledBorderColor,
+                            titleColor: self.disabledTitleColor)
+            
+        case (.secondary, true, false):
+            self.setButtonWith(backgroundColor: self.defaultBackgroundColor,
+                            borderColor: self.defaultBorderColor,
+                            titleColor: self.defaultTitleColor)
+        case (.secondary, true, true):
+            self.setButtonWith(backgroundColor: self.highlightedBackgroundColor,
+                            borderColor: self.highlightedBorderColor,
+                            titleColor: self.highlightedTitleColor)
+        case (.secondary, false, _):
+            self.setButtonWith(backgroundColor: self.disabledBackgroundColor,
+                            borderColor: self.disabledBorderColor,
+                            titleColor: self.disabledTitleColor)
+        }
+    }
+    
+    private func setButtonWith(backgroundColor: UIColor, borderColor: UIColor, titleColor: UIColor) {
         self.backgroundColor = backgroundColor
         layer.borderColor = borderColor.cgColor
         setTitleColor(titleColor, for: .normal)
@@ -221,69 +257,5 @@ class BrandButton: UIButton {
             configuration?.imagePadding = 12
             self.semanticContentAttribute = .forceRightToLeft
         }
-    }
-    
-    private func updateColorStyle() {
-        switch style {
-        case .mainGreen:
-            switch variant {
-            case .primary:
-                defaultBackgroundColor = DS.Colors.Button.defaultGreen
-                defaultBorderColor = .clear
-                defaultTitleColor = DS.Colors.Button.neutralsWhite
-                
-                highlightedBackgroundColor = DS.Colors.Button.highlightedGreen
-                highlightedBorderColor = .clear
-                highlightedTitleColor = DS.Colors.Button.neutralsWhite
-                
-                disabledBackgroundColor = DS.Colors.Button.backgroundDisabled
-                disabledBorderColor = .clear
-                disabledTitleColor = DS.Colors.Button.neutralsWhite
-                
-            case .secondary:
-                defaultBackgroundColor = DS.Colors.Button.neutralsWhite
-                defaultBorderColor = DS.Colors.Button.defaultGreen
-                defaultTitleColor = DS.Colors.Button.defaultGreen
-                
-                highlightedBackgroundColor = DS.Colors.Button.backgroundGreen
-                highlightedBorderColor = DS.Colors.Button.highlightedGreen
-                highlightedTitleColor = DS.Colors.Button.highlightedGreen
-                
-                disabledBackgroundColor = DS.Colors.Button.neutralsWhite
-                disabledBorderColor = DS.Colors.Button.backgroundDisabled
-                disabledTitleColor = DS.Colors.Button.backgroundDisabled
-            }
-            
-        case .mainBlue:
-            switch variant {
-            case .primary:
-                defaultBackgroundColor = DS.Colors.Button.defaultBlue
-                defaultBorderColor = .clear
-                defaultTitleColor = .white
-                
-                highlightedBackgroundColor = DS.Colors.Button.highlightedBlue
-                highlightedBorderColor = .clear
-                highlightedTitleColor = .white
-                
-                disabledBackgroundColor = DS.Colors.Button.backgroundDisabled
-                disabledBorderColor = .clear
-                disabledTitleColor = DS.Colors.Button.neutralsWhite
-                
-            case .secondary:
-                defaultBackgroundColor = .white
-                defaultBorderColor = DS.Colors.Button.defaultBlue
-                defaultTitleColor = DS.Colors.Button.defaultBlue
-                
-                highlightedBackgroundColor = DS.Colors.Button.backgroundBlue
-                highlightedBorderColor = DS.Colors.Button.highlightedBlue
-                highlightedTitleColor = DS.Colors.Button.highlightedBlue
-                
-                disabledBackgroundColor = DS.Colors.Button.neutralsWhite
-                disabledBorderColor = DS.Colors.Button.backgroundDisabled
-                disabledTitleColor = DS.Colors.Button.backgroundDisabled
-            }
-        }
-        
-        updateButtonStyle()
     }
 }
